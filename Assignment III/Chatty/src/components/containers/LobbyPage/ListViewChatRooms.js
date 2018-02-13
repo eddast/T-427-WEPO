d4242db59544aa2de3d9e0246fca826c61cf5761
eddast <eddasr15@ru.is>
2018-02-13 21:29:24 +0000

    CHATROOMS ARE SHOWING

--- Assignment III/Chatty/src/components/containers/LobbyPage/ListViewChatRooms.js
+++ Assignment III/Chatty/src/components/containers/LobbyPage/ListViewChatRooms.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import FontAwesome from 'react-fontawesome';
 
 const ListViewChatRooms = ({ children }) => {
     return (
         <div className="chatroomsActiveList">
-            <p id="ListViewChatroomHeading">CHATROOMS ACTIVE</p>
+            <div>
+                <p id="ListViewChatroomHeading">CHATROOMS ACTIVE <FontAwesome id='addRoomIcon' aria-hidden='false' name='plus'/></p>
+            </div>
             <ul className='listViewChatroom'>{children}</ul>
         </div>
     );
