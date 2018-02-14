6cb957140fe7661c1278bb2df0b811e8f2b554ed
eddast <eddasr15@ru.is>
2018-02-14 00:36:57 +0000

    Can add chatroom

--- Assignment III/Chatty/src/components/containers/LobbyPage/Lobby.js
+++ Assignment III/Chatty/src/components/containers/LobbyPage/Lobby.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ListViewChatRooms from './ListViewChatRooms';
 import ListItemChatRooms from './ListItemChatRooms';
 import ChatRoomWindow from './ChatRoomWindow';
-import Banner from '../../Banner'
-import PropTypes from 'prop-types'
+import Banner from '../../Banner';
+import PropTypes from 'prop-types';
+import Modal from 'react-modal';
 
 class Lobby extends React.Component {
     
@@ -15,7 +16,10 @@ class Lobby extends React.Component {
             chatRoomList : [],
             selectedChatroom: null,
             showChatRoomAvailable : true,
-            shouldreRender: false
+            shouldreRender: false,
+            displayModal: false,
+            newChatroomName: '',
+            newChatroomTopic: ''
         };
         this.server = this.context.serverAPI.server;
         this.server.getChatrooms();
@@ -25,28 +29,55 @@ class Lobby extends React.Component {
         });
     }
 
-    addChatroom() {
-        this.server.addChatroom('f09wejfoiwejfoiwejf', 'mock topic', (didSucceed) => {
-            if(didSucceed) {
-                this.server.getChatrooms();
-                this.server.listenToChatroomUpdates((chatRoomlist) => {
-                    this.setState({chatRoomList: chatRoomlist});
-                });
-            }
+    addChatroomPrompt() {
+        this.setState({displayModal: true});
+        // this.server.addChatroom('f09wejfoiwejfoiwejf', 'mock topic', (didSucceed) => {
+        //     if(didSucceed) {
+        //         this.server.getChatrooms();
+        //         this.server.listenToChatroomUpdates((chatRoomlist) => {
+        //             this.setState({chatRoomList: chatRoomlist});
+        //         });
+        //     }
+        // });
+    }
+
+    updateNewChatroomName(evt) {
+        this.setState({
+            newChatroomName: evt.target.value
         });
+    }
+
+    updateNewChatroomTopic(evt) {
+        this.setState({
+            newChatroomTopic: evt.target.value
+        });
+    }
+
+    closeModalAndAdd() {
+        this.setState({displayModal: false});
+        if(this.state.newChatroomName !== '') {
+            this.server.addChatroom( this.state.newChatroomName, this.state.newChatroomTopic, (didSucceed) => {
+                if(didSucceed) {
+                    this.server.getChatrooms();
+                    this.server.listenToChatroomUpdates((chatRoomlist) => {
+                        this.setState({chatRoomList: chatRoomlist});
+                    });
+                }
+            });
+        }
     }
 
     selectChatroom (evt, chatroom) {
         this.setState({selectedChatroom: chatroom});
     }
 
-    render() {
+    getMainLobbyBody() {
         return (
             <div>
                 <Banner />
                 <div className='LobbyBody'>
                     <div className='chatroomListDisplay'>
-                        <ListViewChatRooms value={this.state.userList} addchatroom={() => this.addChatroom()}>
+                        <ListViewChatRooms value={this.state.userList} addchatroom={() => this.addChatroomPrompt()}>
                             {this.state.chatRoomList.map((chatroom) => (<ListItemChatRooms onClick={evt => this.selectChatroom(evt, chatroom)} value={chatroom} info={chatroom}/>))}
                         </ListViewChatRooms>
                     </div>
@@ -54,6 +85,40 @@ class Lobby extends React.Component {
                 </div>
             </div>
         );
+    }
+
+    getAddChatroomModal() {
+        return (
+            <Modal className='addChatroomPrompt' isOpen='true'>
+                <div className='row'>
+                    <h1>Add Chatroom</h1>
+                    <div className='col-md-10'>
+                        <p>Chatroom Name:</p>
+                        <input onChange={evt => this.updateNewChatroomName(evt)} type='text' className='form-control' placeholder='the Jedi Council'></input>
+                    </div>
+                    <div className='col-md-10'>
+                        <p>Chatroom Topic:</p>
+                        <input onChange={evt => this.updateNewChatroomTopic(evt)} type='text'className='form-control'placeholder='The High Ground'></input>
+                    </div>
+                </div>
+                <div className='row'>
+                    <button onClick={() => this.closeModalAndAdd()}>Create</button>
+                </div>
+            </Modal>
+        );
+    }
+
+    render() {
+        if(this.state.displayModal) {
+            return (
+                <div>
+                    {this.getMainLobbyBody()}
+                    {this.getAddChatroomModal()}
+                </div>
+            );
+        }
+
+        return this.getMainLobbyBody();
     };
 }
 
