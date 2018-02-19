ee38a03e02fea3b5f095764371a63bbd13dec3f2
eddast <eddasr15@ru.is>
2018-02-19 13:41:47 +0000

    options work

--- Assignment III/Chatty/src/components/containers/LobbyPage/ListItemUsers.js
+++ Assignment III/Chatty/src/components/containers/LobbyPage/ListItemUsers.js
@@ -3,19 +3,80 @@ import FontAwesome from 'react-fontawesome';
 
 // Renders a cell in user list view
 // Has an icon indicating user is online and user name
-const ListItemUsers = ({ name, sendPrivateMessage }) => {
-    return (
-        <li className='row userListItem'>
-            <span className='col-md-8'>
-                <FontAwesome id='onlineIcon' name='user'/>
-                <p id='userName'>{name}</p>
-            </span>
-            <span className='col-md-4' id='privateMessageToolTip'>
-                <FontAwesome onClick={() => sendPrivateMessage(name)} id='privateMessageIcon' name='envelope'/>
-                <span className='toolTipText'>Private Message</span>
-            </span>
-        </li>
-    );
+class ListItemUsers extends React.Component {
+
+    constructor(props, ctx) {
+        super(props,ctx);
+        this.state = {
+            seesOptions: false
+        }
+    }
+
+    getListItem() {
+        return (
+            <div className='row userListItem'>
+                <span className='col-md-7'>
+                    <FontAwesome id='onlineIcon' name='user'/>
+                    <p id='userName'>{this.props.name}</p>
+                </span>
+                <span className='col-md-5' id='privateMessageToolTip'>
+                    <FontAwesome onClick={() => this.setState({seesOptions: !this.state.seesOptions})} id='privateMessageIcon' name='ellipsis-h'/>
+                    <span className='toolTipText'>Options</span>
+                </span>
+            </div>
+        );
+    }
+
+    getOptions() {
+        return (
+            <div id='userOptions'>
+                <div className='row'>
+                    <div className='col-md-offset-1 col-md-1'>
+                        <FontAwesome onClick={() => this.props.sendPrivateMessage(this.props.name)} id="privateMessageIcon" name="envelope"/>
+                    </div>
+                    <div className='col-md-offset-1 col-md-1'>
+                        <FontAwesome onClick={() => alert('HAH ban!')} id='banIcon' name='ban'/>
+                    </div>
+                    <div className='col-md-offset-1 col-md-1'>
+                        <FontAwesome onClick={() => alert('HAH kick!')} id='banIcon' name='sign-out'/>
+                    </div>
+                    <div className='col-md-offset-1 col-md-1'>
+                        <FontAwesome onClick={() => this.setState({seesOptions: !this.state.seesOptions})} id='banIcon' name='times-circle'/>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-md-offset-1 col-md-1'>
+                        <p className='optionsDescription'>Private Convo</p>
+                    </div>
+                    <div className='col-md-offset-1 col-md-1'>
+                        <p className='optionsDescription'>Ban</p>
+                    </div>
+                    <div className='col-md-offset-1 col-md-1'>
+                        <p className='optionsDescription'>Kick</p>
+                    </div>
+                    <div className='col-md-offset-1 col-md-1'>   
+                        <p className='optionsDescription'>Hide options</p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    render() {
+        if(this.state.seesOptions) {
+            return (
+                <li>
+                    {this.getListItem()}
+                    {this.getOptions()}
+                </li>
+            );
+        }
+        return (
+            <li>
+                {this.getListItem()}
+            </li>
+        );
+    }
 };
 
 export default ListItemUsers;
