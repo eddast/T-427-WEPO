b851b94eb20a477019bad6477b46cbaeaf542801
eddast <eddasr15@ru.is>
2018-02-20 15:01:26 +0000

    nicknamechoice test

--- Assignment III/Chatty/src/components/containers/InitialPageNickname/NicknameChoice/NicknameChoice.spec.js
+++ Assignment III/Chatty/src/components/containers/InitialPageNickname/NicknameChoice/NicknameChoice.spec.js
@@ -1,13 +1,41 @@
 import React from "react";
 import { shallow } from "enzyme";
-import NickNameChoice from "./NicknameChoice";
+import NicknameChoice from "./NicknameChoice";
+import { SocketIO, Server } from "mock-socket";
+import Service from '../../../../services/API';
+import { Redirect } from 'react-router';
 
 jest.useFakeTimers();
 
-describe("List Item tests", () => {
+describe("Nickname Prompt Tests", () => {
 
-  it("should be 12", () => {
-    expect(12).toBe(12);
+  // Create mock server and socket
+  let mockSocketServer = new Server("http://localhost:8080");
+  let mockSocket = SocketIO.connect("http://localhost:8080");
+
+  // Tests whether input value updates correctly
+  it("Update Input Value", () => {
+    const server = Service;
+    server.connect();
+    const context = {
+      serverAPI : {
+        server: server
+      },
+      routeTools : {
+        redirect: Redirect
+      }
+    }
+    const component = shallow(<NicknameChoice/>, { context });
+    console.log(component.find('.inputNickname'));
+    component.find('input').simulate('change', {target: {value: 'nickname'}});
+    expect(component.state().inputValue).toEqual('nickname');
   });
+
+  // Destroy mock server and socket
+  afterEach(() => {
+    mockSocketServer.stop();
+    mockSocket.close();
+  });
+
 
 });
