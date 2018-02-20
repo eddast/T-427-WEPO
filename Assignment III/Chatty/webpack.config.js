5df15c0eda9fc0e72c284d84f2a44bc0351ffa4d
eddast <eddasr15@ru.is>
2018-02-20 13:56:46 +0000

    folder struct + test files

--- Assignment III/Chatty/webpack.config.js
+++ Assignment III/Chatty/webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
-        port: 9020,
+        port: 9000,
         open: true
     },
     devtool: 'inline-source-map',
