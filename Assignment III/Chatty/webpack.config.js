75a47094c5a6089d97e2b8b1e0db2ffc94b50d56
unknown <stulli103@gmail.com>
2018-02-20 11:17:40 +0000

    Ohh yeah

--- Assignment III/Chatty/webpack.config.js
+++ Assignment III/Chatty/webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
-        port: 9008,
+        port: 9009,
         open: true
     },
     devtool: 'inline-source-map',
