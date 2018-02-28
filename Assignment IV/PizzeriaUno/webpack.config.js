0169fa1ed698c153bcd8c4278f515fc175b3cd19
eddast <eddasr15@ru.is>
2018-02-28 02:12:48 +0000

    Project sceleton for ASS3

---
+++ Assignment IV/PizzeriaUno/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            { enforce: 'pre', test: /\.js$/, exclude: /node_modules/, use: 'eslint-loader' },
+            { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' },
+            { test: /\.less$/, use: [
+                { loader: 'style-loader' },
+                { loader: 'css-loader' },
+                { loader: 'less-loader' }
+            ]},
+            { test: /\.svg$/,
+                use: [
+                    { loader: "babel-loader"},
+                    {
+                        loader: "react-svg-loader",
+                        options: { jsx: true }
+                    }
+                ]
+            },
+            { test: /(\.css$)/, loaders: ['style-loader', 'css-loader', 'postcss-loader'] },
+            { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
+        ]
+    },
+    watch: true,
+    resolve: {
+        extensions: ['.js', '.jsx', '.less']
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+        open: true
+    },
+    devtool: 'inline-source-map',
+    plugins: [new HtmlWebpackPlugin({
+        title: 'PizzeriaUno',
+        template: './index.html'
+    })]
+};
