166ae8b276285cf0bd1cfff4562b6271227555af
eddast <eddasr15@ru.is>
2018-02-28 03:43:12 +0000

    Project sceleton for ASS3

--- Assignment IV/PizzeriaUno/src/app.js
+++ Assignment IV/PizzeriaUno/src/app.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../styles/site';
-import Logo from './resources/PizzeriaUno.png';
+import InitialPage from './components/InitialPage/InitialPage';
+// import LoadingScreen from './components/LoadingScreen/LoadingScreen';
 
 class App extends React.Component {
     render() {
-        return(
-            <div className='initialPage'>
-                <div className='initialPageContainer'>
-                    <h1 id='greeting'>B e n v e n u t o &nbsp; a &nbsp;</h1>
-                    <img id='logo' src={Logo} alt='logo'/>
-                    <h2 id='tagline'>"Love at first bite"</h2>
-                    <div className='optionBar'>
-                        <span className='options'>M e n u</span>
-                        <span className='options'>O f f e r s</span>
-                        <span className='options'>A b o u t &nbsp; U s</span>
-                        <span className='options'>C a r t</span>
-                    </div>
-                </div>
-            </div>
-        );
+        return <InitialPage />;
+        // return <LoadingScreen />
     }
 }
 
