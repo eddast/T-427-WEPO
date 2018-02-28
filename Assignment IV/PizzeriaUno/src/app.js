c99d98fedc3f8136e026f92ae78f0e1d936c0f8c
eddast <eddasr15@ru.is>
2018-02-28 02:16:00 +0000

    Project sceleton for ASS3

--- Assignment IV/PizzeriaUno/src/app.js
+++ Assignment IV/PizzeriaUno/src/app.js
@@ -7,14 +7,16 @@ class App extends React.Component {
     render() {
         return(
             <div className='initialPage'>
-                <h1 id='greeting'>B e n v e n u t o &nbsp; a &nbsp;</h1>
-                <img id='logo' src={Logo} alt='logo'/>
-                <h2 id='tagline'>"Love at first bite"</h2>
-                <div className='optionBar'>
-                    <span className='options'>M e n u</span>
-                    <span className='options'>O f f e r s</span>
-                    <span className='options'>A b o u t &nbsp; U s</span>
-                    <span className='options'>C a r t</span>
+                <div className='initialPageContainer'>
+                    <h1 id='greeting'>B e n v e n u t o &nbsp; a &nbsp;</h1>
+                    <img id='logo' src={Logo} alt='logo'/>
+                    <h2 id='tagline'>"Love at first bite"</h2>
+                    <div className='optionBar'>
+                        <span className='options'>M e n u</span>
+                        <span className='options'>O f f e r s</span>
+                        <span className='options'>A b o u t &nbsp; U s</span>
+                        <span className='options'>C a r t</span>
+                    </div>
                 </div>
             </div>
         );
