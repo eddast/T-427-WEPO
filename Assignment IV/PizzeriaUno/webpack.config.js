2a9fb4e89f904e8c69593003e943580ecba0788b
eddast <eddasr15@ru.is>
2018-03-08 19:06:26 +0000

    fix port numbers

--- Assignment IV/PizzeriaUno/webpack.config.js
+++ Assignment IV/PizzeriaUno/webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
-        port: 9011,
+        port: 9000,
         open: true
     },
     devtool: 'inline-source-map',
