2a65a3141d1182245e1450be18001e02ba465468
unknown <stulli103@gmail.com>
2018-03-08 18:54:44 +0000

    Small testing fixes

--- Assignment IV/PizzeriaUno/src/components/Checkout/CheckoutPageView/CheckoutPageView.spec.js
+++ Assignment IV/PizzeriaUno/src/components/Checkout/CheckoutPageView/CheckoutPageView.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Router, Route } from 'react-router';
+// import { Router, Route } from 'react-router';
 import CheckoutPageView from './CheckoutPageView';
 import NavigationBar from '../../NavigationBar/NavigationBar';
 
@@ -10,33 +10,30 @@ jest.useFakeTimers();
 // It's only purpose is to contain necessary visual things
 describe('CheckoutPageView dummy render tests', () => {
 
-  describe('should render background, navbar, correct message prompt, and two links (delivery and pickup links)', () => {
-    const component = shallow(<CheckoutPageView />);
+    describe('should render background, navbar, correct message prompt, and two links (delivery and pickup links)', () => {
+        const component = shallow(<CheckoutPageView />);
 
-    it('should have a delicious pizza background', () => {
-        expect(component.find('.pizzaBackground').length == 1).toBe(true);
+        it('should have a delicious pizza background', () => {
+            expect(component.find('.pizzaBackground').length == 1).toBe(true);
+        });
+
+        it('should have a delicious pizza background', () => {
+            expect(component.contains(<NavigationBar />)).toBe(true);
+        });
+
+        it('should render two delivery options, pickup and delivery', () => {
+            expect(component.find('.showOptionsForCheckoutText').contains('Delivery')).toBe(true);
+            expect(component.find('.showOptionsForCheckoutText').contains('Pick Up')).toBe(true);
+        });
+
+        // TODO test link
+        it('should redirect to /checkout/delivery when clicked on delivery link', () => {
+            expect(component.find('#deliveryLink').length==1).toBe(true);
+        });
+
+        // TODO test link
+        it('should redirect to /checkout/pickup when clicked on pickup link', () => {
+            expect(component.find('#pickupLink').length==1).toBe(true);
+        });
     });
-
-    it('should have a delicious pizza background', () => {
-        expect(component.contains(<NavigationBar />)).toBe(true);
-    });
-
-    it('should render two delivery options, pickup and delivery', () => {
-        expect(component.find('.showOptionsForCheckoutText').contains('Delivery')).toBe(true);
-        expect(component.find('.showOptionsForCheckoutText').contains('Pick Up')).toBe(true);
-    });
-
-    // TODO test link
-    it('should redirect to /checkout/delivery when clicked on delivery link', () => {
-        expect(component.find('#deliveryLink').length==1).toBe(true);
-    });
-
-    // TODO test link
-    it('should redirect to /checkout/pickup when clicked on pickup link', () => {
-        expect(component.find('#pickupLink').length==1).toBe(true);
-    });
-
-
-  });
-
 });
--- Assignment IV/PizzeriaUno/src/components/Checkout/ConfirmOrder/ConfirmOrder.spec.js
+++ Assignment IV/PizzeriaUno/src/components/Checkout/ConfirmOrder/ConfirmOrder.spec.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import { shallow } from 'enzyme';
 import NavigationBar from '../../NavigationBar/NavigationBar';
 import ConfirmOrder from './ConfirmOrder';
@@ -9,32 +9,32 @@ jest.useFakeTimers();
 // ConfirmOrder component is a dummy component - i.e. no logic really
 // It's only purpose is to contain the navigation bar and an explicit confirmation message
 describe('ConfirmOrder dummy render tests', () => {
-  const component = shallow(<ConfirmOrder/>);
+    const component = shallow(<ConfirmOrder/>);
 
-  it('should have a delicious pizza background', () => {
-    expect(component.find('.pizzaBackground').length).toBe(1);
-  });
+    it('should have a delicious pizza background', () => {
+        expect(component.find('.pizzaBackground').length).toBe(1);
+    });
 
-  it('should contain the NavigationBar component', () => {
-    expect(component.contains (<NavigationBar/>)).toBe(true);
-  });
+    it('should contain the NavigationBar component', () => {
+        expect(component.contains (<NavigationBar/>)).toBe(true);
+    });
 
-  it('should notify user pizza is going in oven', () => {
-    expect(component.find('.confirmationBody').contains('PIZZA IS GOING IN THE OVEN ')).toBe(true);
-  });
+    it('should notify user pizza is going in oven', () => {
+        expect(component.find('.confirmationBody').contains('PIZZA IS GOING IN THE OVEN ')).toBe(true);
+    });
 
-  it('should exite user about his or her pizza!', () => {
-    expect(component.find('.confirmationBody').contains('In just a couple of minutes you\'ll get your promised italian gourmet feast!')).toBe(true);
-  });
+    it('should exite user about his or her pizza!', () => {
+        expect(component.find('.confirmationBody').contains('In just a couple of minutes you\'ll get your promised italian gourmet feast!')).toBe(true);
+    });
 
-  it('should tell user pizza is loading', () => {
-    expect(component.find('.confirmationBody').contains('Loading pizza...')).toBe(true);
-  });
+    it('should tell user pizza is loading', () => {
+        expect(component.find('.confirmationBody').contains('Loading pizza...')).toBe(true);
+    });
 
-  it('should display a loading image to indicate pizza progress', () => {
-    expect(component.find('#loadImage').filterWhere((item) => {
-        return item.prop('src') === Loader;
-    }).length==1).toBe(true);
-  });
+    it('should display a loading image to indicate pizza progress', () => {
+        expect(component.find('#loadImage').filterWhere((item) => {
+            return item.prop('src') === Loader;
+        }).length==1).toBe(true);
+    });
 
 });
