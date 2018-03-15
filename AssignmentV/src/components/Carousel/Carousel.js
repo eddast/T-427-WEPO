82f12dc90e8c268c58a9f8b3cb02b8fcbb2870c1
Darri Valgarðsson <LAVDAR@li01r1d.lais.net>
2018-03-15 17:58:10 +0000

    arrows for carousel

--- AssignmentV/src/components/Carousel/Carousel.js
+++ AssignmentV/src/components/Carousel/Carousel.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from "prop-types";
 import styles from "./carousel.css";
+import FontAwesome from "react-fontawesome"
 
 class Carousel extends React.Component {
     constructor(props) {
@@ -32,9 +33,11 @@ class Carousel extends React.Component {
         const { currentImage } = this.state;
         return (
             <div className={`${styles.carousel} ${styles[`carousel-${this.props.size}`]} `}>
-                <div className={`${ (styles.fade)} ${styles[`image-${this.props.size}`]}`} >
-                    <img src={images[currentImage]} alt="Loading"/>
-                </div>
+                <div style={{ backgroundImage: `url(${images[currentImage]})`}} className={`${ (styles.fade)} ${styles[`image-${this.props.size}`]}`} >
+                    <FontAwesome aria-hidden='false' size='2x' name='chevron-circle-left' onClick={()=> { if(currentImage > 0){ this.setState({currentImage: currentImage-1})}} }/>
+                    {/* <img src={images[currentImage]} alt="Loading"/> */}
+                    <FontAwesome className={`${ (styles.arrow)}`} aria-hidden='false' size='2x' name='chevron-circle-right' onClick={()=> { if(currentImage < images.length-1){this.setState({currentImage: currentImage+1})}} }/>
+                </div>          
                 <div className={`${styles.dots}`}>{dots}</div>
             </div>
         );
