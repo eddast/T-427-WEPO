70c6dbd42a59148a9626804e73864dfc65dc3675
darri19 <darri19@gmail.com>
2018-03-15 22:05:19 +0000

    Carousel lookin good

--- AssignmentV/src/components/Carousel/Carousel.js
+++ AssignmentV/src/components/Carousel/Carousel.js
@@ -22,7 +22,7 @@ class Carousel extends React.Component {
         if (images.length > 1){
             dots = images.map((i, idx) => (
                 <span
-                    className={styles.dot}
+                    className={`${styles.dot}` + (this.state.currentImage === idx ? ` ${styles.selectedDot}` : ``)}
                     key={idx}
                     onClick={e => this.changePicture(e, idx)}
                 />
@@ -34,10 +34,18 @@ class Carousel extends React.Component {
         return (
             <div className={`${styles.carousel} ${styles[`carousel-${this.props.size}`]} `}>
                 <div style={{ backgroundImage: `url(${images[currentImage]})`}} className={`${ (styles.fade)} ${styles[`image-${this.props.size}`]}`} >
-                    <FontAwesome aria-hidden='false' size='2x' name='chevron-circle-left' onClick={()=> { if(currentImage > 0){ this.setState({currentImage: currentImage-1})}} }/>
-                    {/* <img src={images[currentImage]} alt="Loading"/> */}
-                    <FontAwesome className={`${ (styles.arrow)}`} aria-hidden='false' size='2x' name='chevron-circle-right' onClick={()=> { if(currentImage < images.length-1){this.setState({currentImage: currentImage+1})}} }/>
-                </div>          
+                    <FontAwesome className={`${ (styles.arrow)} ${ (styles['arrow-left'])}`} aria-hidden='false' size='3x' name='chevron-circle-left' onClick={()=> { if(currentImage > 0){ this.setState({currentImage: currentImage-1})}} }/>
+                    <FontAwesome className={`${ (styles.arrow)} ${ (styles['arrow-right'])}`} aria-hidden='false' size='3x' name='chevron-circle-right' onClick={()=> { if(currentImage < images.length-1){this.setState({currentImage: currentImage+1})}} }/>
+                </div>
+                {/* <ul className={`${styles['slide-select']} `}>
+                    <li className={`${styles['btn prev']} `}></li>
+                    <li className={`${styles['selector']} `}></li>
+                    <li className={`${styles['selector']} `}></li>
+                    <li className={`${styles['selector']} `}></li>
+                    <li className={`${styles['selector']} `}></li>
+                    <li className={`${styles['selector']} `}></li>
+                    <li className={`${styles['btn next']} `}></li>
+                </ul>           */}
                 <div className={`${styles.dots}`}>{dots}</div>
             </div>
         );
