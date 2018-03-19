986b2b138029aa4b0aa5b02a688fc16437445757
eddast <eddasr15@ru.is>
2018-03-19 00:08:21 +0000

    made carousel WAY WAY WAY WAY WAY WAY WAY WAY MORE BEAUTIFUL

--- AssignmentV/src/components/GlowBox/GlowBox.js
+++ AssignmentV/src/components/GlowBox/GlowBox.js
@@ -3,25 +3,28 @@ import propTypes from 'prop-types';
 import styles from './glowbox.css';
 import FontAwesome from "react-fontawesome";
 
-const GlowBox = ({onClick, icon, children, modal}) => {
+/**
+ * Renders a glowing input-like box with an onClick function
+ * Has an icon to the right which hints user as to what that functionality is  
+ */
+const GlowBox = ({onClick, icon, children}) => {
         return (
             <div className={`${styles.wrapper}`}>
                 <div className={`${styles.content}`} onClick={()=> onClick()}>
                     <FontAwesome className={`${styles.icon}`} aria-hidden='false' name={icon} />
                     {children}
                 </div>
-                {modal}
             </div>
         );
 };
 
+// Props glowbox needs to function
 GlowBox.propTypes = {
+    /* Onclick action once box is clicked */
     onClick: propTypes.func.isRequired,
+    /* Icon hinting user which action is taken on click */
     icon: propTypes.string.isRequired
 };
 
-GlowBox.defaultProps = {
-    modal: ''
-};
 
 export default GlowBox
