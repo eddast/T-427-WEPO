bb85237f827828488e129c8ce7470a77d3aed6a2
eddast <eddasr15@ru.is>
2018-03-19 00:04:05 +0000

    made carousel WAY WAY WAY WAY WAY WAY WAY WAY MORE BEAUTIFUL

--- AssignmentV/src/App.js
+++ AssignmentV/src/App.js
@@ -60,17 +60,18 @@ class App extends React.Component {
         </div>
         <div className={`${styles["container-item"]}`}>
           <h2>4.Carousel Component Demo</h2>
+          <p> Jared Padalecki is very hot yes </p>
           <div>
-            <h3>Small</h3>
-            {this.testCarousel(["https://www.w3schools.com/w3css/img_nature_wide.jpg", "https://www.w3schools.com/w3css/img_mountains_wide.jpg", "https://www.w3schools.com/w3css/img_fjords_wide.jpg"], "small")}
+            <h3>4.1. Small</h3>
+            {this.testCarousel(["http://7wallpapers.net/wp-content/uploads/16_Jared-Padalecki.jpg","http://7wallpapers.net/wp-content/uploads/1_Jared-Padalecki.jpg", "http://7wallpapers.net/wp-content/uploads/2_Jared-Padalecki.jpg", "http://7wallpapers.net/wp-content/uploads/5_Jared-Padalecki.jpg"], "small")}
           </div>
           <div>
-            <h3>Medium</h3>
-            {this.testCarousel(["https://www.w3schools.com/w3css/img_nature_wide.jpg", "https://www.w3schools.com/w3css/img_mountains_wide.jpg", "https://www.w3schools.com/w3css/img_fjords_wide.jpg"], "medium")}
+            <h3>4.2. Medium</h3>
+            {this.testCarousel(["http://7wallpapers.net/wp-content/uploads/16_Jared-Padalecki.jpg", "http://7wallpapers.net/wp-content/uploads/1_Jared-Padalecki.jpg", "http://7wallpapers.net/wp-content/uploads/2_Jared-Padalecki.jpg", "http://7wallpapers.net/wp-content/uploads/5_Jared-Padalecki.jpg"], "medium")}
           </div>
           <div>
-            <h3>Large</h3>
-            {this.testCarousel(["https://www.w3schools.com/w3css/img_nature_wide.jpg", "https://www.w3schools.com/w3css/img_mountains_wide.jpg", "https://www.w3schools.com/w3css/img_fjords_wide.jpg"], "large")}
+            <h3>4.3. Large</h3>
+            {this.testCarousel(["http://7wallpapers.net/wp-content/uploads/16_Jared-Padalecki.jpg", "http://7wallpapers.net/wp-content/uploads/1_Jared-Padalecki.jpg", "http://7wallpapers.net/wp-content/uploads/2_Jared-Padalecki.jpg", "http://7wallpapers.net/wp-content/uploads/5_Jared-Padalecki.jpg"], "large")}
           </div>  
         </div>
         <div className={`${styles["container-item"]}`}>
